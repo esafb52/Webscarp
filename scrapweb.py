0660992cebadce371272ecadbc2f71743e4a10d4
Masiha Ahmadi <esafb52@users.noreply.github.com>
2018-04-14 22:59:01 +0430

    edit some code

--- scrapweb.py
+++ scrapweb.py
@@ -3,20 +3,19 @@ import re
 import bs4
 import requests
 import wget
+import random
 
 
 def main():
-    # web address to download files
     def_url = 'http://www.m-afshar.net/ganjineye%20adabi.htm'
-    # get urls and farsi names  list form links
     res_lst, farsinames = get_file_link(def_url)
     lb: str
     for li, lb in zip(res_lst, farsinames):
         try:
-            # download file one by one
             downlodfile(li, lb)
-        except:
-            print('error to downloa file {0}'.format(lb))
+        except Exception as e:
+            print('error to download file {0} because {1}'.format(lb,e))
+
     print('***    download complete !!!!   ***')
 
 
@@ -29,38 +28,39 @@ def get_file_link(url):
     links = res_bs4.find_all('a', href=True)
     for li in links:
         finl_res = str(li.get('href'))
-        # find custome extions ( '.doc', '.docx', '.pdf') in links
         if finl_res.endswith(('.doc', '.docx', '.pdf')):
             t = str(li.text)
             sentence = " ".join(re.split("\s+", t, flags=re.UNICODE))
             lst_name.append(sentence)
             lst_file.append(finl_res)
+
     return lst_file, lst_name
 
 
 def downlodfile(url, farsiname):
-    # for fix borken link
-    fixlink = 'http://www.m-afshar.net/'
-    # set place to store file in pc
-    out_folder_1 = 'e:/t/books/'
+
+    fixing = 'http://www.m-afshar.net/'
+    out_folder_1 = 'e:/book/'
+    out_folder_2 = 'e:/book/error/'
     try:
         my_url = str(url)
         name = os.path.basename(my_url)
-        # set  path and ext for evey file
+
         if name.endswith('.pdf'):
             fixname = out_folder_1 + name.replace(name, farsiname + ".pdf")
         elif name.endswith('.docx'):
             fixname = out_folder_1 + name.replace(name, farsiname + ".docx")
         else:
             fixname = out_folder_1 + name.replace(name, farsiname + ".doc")
-        # fix broken links
+
         if my_url.startswith('future'):
-            my_url = fixlink + my_url
+            my_url = fixing + my_url
         print("*** download start : {}   *** ".format(my_url))
-        # download file
         wget.download(my_url, fixname)
-    except:
-        print('downlod error {} file!!!'.format(my_url))
+    except Exception as e:
+        tem=str(random.randint(0,10000))
+        wget.download(my_url,out_folder_2+tem+'.doc')
+        print('downlod error {} file!!! because {}'.format(my_url,e))
 
 
 if __name__ == '__main__':
